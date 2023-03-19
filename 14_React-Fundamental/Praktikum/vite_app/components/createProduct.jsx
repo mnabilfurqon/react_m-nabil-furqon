a0a0afd37e231ea21a3355ef52a5f00b97624f5c
M Nabil Furqon <mnabilfurqon71@gmail.com>
2023-03-19 18:06:40 +0700

    Update praktikum

---
+++ 14_React-Fundamental/Praktikum/vite_app/components/createProduct.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const CreateProduct = () => {
+  return (
+    <div>
+      <h1>Create Product</h1>
+      <h2>Detail Product</h2>
+      <label>Product Name</label> : <br />
+      <input type="text" /> <br />
+      <br />
+      <label>Product Category :</label> <br />
+      <select>
+        <option />
+        <option />
+        <option />
+      </select>{" "}
+      <br />
+      <br />
+      <label>Product Freshness</label> : <br />
+      <input type="radio" name="pf" /> Brand New <br />
+      <input type="radio" name="pf" /> Second Hank <br />
+      <input type="radio" name="pf" /> Refurbished <br />
+      <br />
+      <label>
+        Image of Product <input type="file" />
+      </label>{" "}
+      <br />
+      <br />
+      <label>Additional Description</label>
+      <br />
+      <textarea rows={10} cols={30} defaultValue={""} /> <br />
+      <br />
+      <label>Product Price :</label>
+      <br />
+      <input type="number" placeholder="$ 100" /> <br />
+      <br />
+      <button type="submit">Submit</button>
+    </div>
+  );
+}
+
+export default CreateProduct;
--- 14_React-Fundamental/Praktikum/vite_app/src/App.jsx
+++ 14_React-Fundamental/Praktikum/vite_app/src/App.jsx
@@ -1,45 +1,14 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import CreateProduct from '../components/createProduct'
+import Route from '../router/route'
 // import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Create Product</h1>
-      <h2>Detail Product</h2>
-      <label>Product Name</label> : <br />
-      <input type="text" /> <br />
-      <br />
-      <label>Product Category :</label> <br />
-      <select>
-        <option />
-        <option />
-        <option />
-      </select>{" "}
-      <br />
-      <br />
-      <label>Product Freshness</label> : <br />
-      <input type="radio" name="pf" /> Brand New <br />
-      <input type="radio" name="pf" /> Second Hank <br />
-      <input type="radio" name="pf" /> Refurbished <br />
-      <br />
-      <label>
-        Image of Product <input type="file" />
-      </label>{" "}
-      <br />
-      <br />
-      <label>Additional Description</label>
-      <br />
-      <textarea rows={10} cols={30} defaultValue={""} /> <br />
-      <br />
-      <label>Product Price :</label>
-      <br />
-      <input type="number" placeholder="$ 100" /> <br />
-      <br />
-      <button type="submit">Submit</button>
+      <Route path="/">
+        <CreateProduct></CreateProduct>
+      </Route>
     </div>
   )
 }
